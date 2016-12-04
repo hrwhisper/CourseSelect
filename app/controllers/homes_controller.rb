9b5bec2458ce624898bc027276a38a4e909c0ec5
Ruijie Liao <liaoruijie15@mails.ucas.ac.cn>
2016-12-04 13:08:07 +0000

    show course without login -- Jarily

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,23 @@
 class HomesController < ApplicationController
 
   def index
+    @course=Course.where('open = true')
+    # @course=@course-current_user.courses
+  end
+  
+# Confirms a student logged-in user.
+  
+  def student_logged_in
+    unless student_logged_in?
+      redirect_to root_url, flash: {danger: '请登陆'}
+    end
   end
 
+  # Confirms a teacher logged-in user.
+  def teacher_logged_in
+    unless teacher_logged_in?
+      redirect_to root_url, flash: {danger: '请登陆'}
+    end
+  end
+  
 end
