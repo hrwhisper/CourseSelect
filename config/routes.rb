45f5208e59271d21910725d887fc6867b15203ee
longyueying <2726012413@qq.com>
2016-12-25 13:06:51 +0000

    教师查看评教结果

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,14 @@ Rails.application.routes.draw do
 
   resources :grades, only: [:index, :update]
   resources :users
-  resources :comments
+  resources :comments do
+    member do
+    end
+    collection do
+      get :list
+    end
+  end
+  
 
   get 'sessions/login' => 'sessions#new'
   post 'sessions/login' => 'sessions#create'
