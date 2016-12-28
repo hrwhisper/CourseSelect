ac85648cabc49e858ea6e186b79f097ab83205fe
longyueying <2726012413@qq.com>
2016-12-28 03:28:26 +0000

    评估数据验证

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,4 @@
 class Comment < ActiveRecord::Base
     belongs_to :course
     belongs_to :user
-    
-    validates :comment_1,:comment_2,:comment_3,:comment_4,:comment_5,:comment_6,:comment_7,:comment_8,:comment_9,:comment_10,
-    :comment_11,:comment_12,:comment_13,:comment_14,:comment_15,:comment_16,:comment_17, presence: true
 end
